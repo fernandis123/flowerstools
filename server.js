553efd13edc11ad25413ba5b61c0534203290e00
fernandis123 <fernandis5656@163.com>
2026-06-26 15:48:28 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// 静态文件
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-// 首页路由（兜底）
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
